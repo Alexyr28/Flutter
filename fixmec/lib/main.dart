c03bbd07db51389cf244e98d938788ae876a9ee3
Alexyr28 <alexyr24@gmail.com>
2025-10-10 15:46:24 -0500

    feat: SplashScreen

--- fixmec/lib/main.dart
+++ fixmec/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-import 'package:fixmec/pages/ui/home.dart';
 import 'package:flutter/material.dart';
 import 'package:fixmec/pages/ui/splash_screen.dart';
 
@@ -7,29 +5,12 @@ void main() {
   runApp(const FixMec());
 }
 
-class FixMec extends StatefulWidget {
+class FixMec extends StatelessWidget {
   const FixMec({super.key});
 
   @override
-  State<FixMec> createState() => _FixMecState();
-}
-
-class _FixMecState extends State<FixMec> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(
-      Duration(seconds: 3),
-      () => Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const HomeFixMec()),
-      ),
-    );
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SplashScreenFixMec(),
     );
--- fixmec/lib/pages/ui/splash_screen.dart
+++ fixmec/lib/pages/ui/splash_screen.dart
@@ -1,10 +1,22 @@
+import "package:animated_splash_screen/animated_splash_screen.dart";
+import "package:fixmec/pages/ui/home.dart";
 import "package:flutter/material.dart";
+import "package:lottie/lottie.dart";
 
 class SplashScreenFixMec extends StatelessWidget {
   const SplashScreenFixMec({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.blueAccent);
+    return AnimatedSplashScreen(
+      splash: Center(
+        child: Lottie.asset("assets/animation/Happy Mechanic.json"),
+      ),
+      nextScreen: HomeFixMec(),
+      splashTransition: SplashTransition.fadeTransition,
+      duration: 3500,
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      splashIconSize: 400,
+    );
   }
 }
