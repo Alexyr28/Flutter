0e53c3996ff06d0d88dc5ee03f0b5a75782f7158
DanielTrujillo5 <danieltigo555@gmail.com>
2025-10-13 17:07:01 -0500

    Cambios en el carrusel y tamaño de imagen

--- fixmec/lib/pages/ui/home.dart
+++ fixmec/lib/pages/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HomeFixMec extends StatelessWidget {
   const HomeFixMec({super.key});
@@ -8,7 +9,7 @@ class HomeFixMec extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(5.0),
           child: Image.asset("assets/icons/repair.png", fit: BoxFit.contain),
         ),
         title: const Text(
@@ -31,7 +32,81 @@ class HomeFixMec extends StatelessWidget {
         ),
         backgroundColor: Colors.transparent,
       ),
-      body: SafeArea(child: const Center(child: Text("Bienvenido a FixMec"))),
+
+      // cuerpo
+      body: Container(
+        // 🔹 Fondo con gradiente
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFFF4E2D8), Color(0xFFECE9E6)],
+          ),
+        ),
+
+        child: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+
+              // carrusel
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 190.0,
+                  viewportFraction: 0.8,
+                  initialPage: 0,
+                  enableInfiniteScroll: true,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 3),
+                  autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enlargeCenterPage: true,
+                  enlargeFactor: 0.25,
+                  scrollDirection: Axis.horizontal,
+                ),
+
+                // img de las motos
+                items: ["assets/motos/apache.png", "assets/motos/evo.png"].map((
+                  imgPath,
+                ) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 5.0,
+                          vertical: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.15),
+                              blurRadius: 6,
+                              offset: const Offset(0, 4),
+                            ),
+                          ],
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Image.asset(
+                              imgPath,
+                              fit: BoxFit.contain, //tamaño imag
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
