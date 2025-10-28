2d94a5232399eefef37147f687db0a2fb85a2a70
DanielTrujillo5 <danieltigo555@gmail.com>
2025-10-28 07:36:12 -0500

    feat:tamaño

--- fixmec/lib/pages/ui/inicio.dart
+++ fixmec/lib/pages/ui/inicio.dart
@@ -114,7 +114,7 @@ class _InicioFixMec extends State<InicioFixMec> {
             crossAxisSpacing: 12,
             shrinkWrap: true,
             physics: const NeverScrollableScrollPhysics(),
-            childAspectRatio: 2.0,
+            childAspectRatio: 1.7,
             children: [
               PercentFixMec(
                 title: loc.translate("oil"),
