17e3a68b175e7e897a9e4285de14b38d6476b3ed
Alexyr28 <alexyr24@gmail.com>
2025-11-24 22:17:42 -0500

    Revert "feat: NotificacionesOK"
    This reverts commit 33876b375d19181b13a57b1c174bafaf6123b9b9.

--- fixmec/lib/main.dart
+++ fixmec/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fixmec/services/background_task.dart';
+// import 'package:fixmec/services/background_task.dart';
 import 'package:fixmec/services/notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
